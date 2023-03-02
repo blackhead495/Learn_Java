db30151ec5d9c0cc8c42784144d9263d8a16b495
Profi <faris-d@mail.ru>
2023-03-02 20:47:36 +0300

    Создал тестовый проект

---
+++ Webinars/Webinar1/Test1.java
@@ -0,0 +1,10 @@
+package Webinars.Webinar1;
+
+import java.time.LocalDateTime;
+
+public class Test1 {
+    public static void main(String[] args) {
+        LocalDateTime nowDate = LocalDateTime.now();
+        System.out.println(nowDate);
+    }
+}
