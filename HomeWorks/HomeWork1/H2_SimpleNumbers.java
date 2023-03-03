dbf1c15f4fdd493308288d1feeaa29c0f61e5d15
Profi <faris-d@mail.ru>
2023-03-03 22:52:53 +0300

    Решил домашнюю задачу 2

---
+++ HomeWorks/HomeWork1/H2_SimpleNumbers.java
@@ -0,0 +1,22 @@
+// Вывести все простые числа от 1 до 1000
+
+package HomeWorks.HomeWork1;
+
+public class H2_SimpleNumbers {
+    public static void main(String[] args) {
+        System.out.print("Простые числа: ");
+        for (int i = 1; i <= 1000; i++) {
+            if (SimpleDig(i)) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+    public static boolean SimpleDig(int dig) {
+        for (int i = 2; i < dig; i++) {
+            if (dig % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
