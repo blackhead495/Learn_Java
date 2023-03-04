00cf9858d7d9823f0c3acbb4f95009a74ca9e85a
Profi <faris-d@mail.ru>
2023-03-04 10:37:51 +0300

    Решил домашнюю задачу 3

---
+++ HomeWorks/HomeWork1/H3_Calculator.java
@@ -0,0 +1,32 @@
+package HomeWorks.HomeWork1;
+import java.util.Scanner;
+public class H3_Calculator {
+    public static void main(String[] args) {
+        double num1;
+        double num2;
+        double ans;
+        char op;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Введите 1 число: ");
+        num1 = reader.nextDouble();
+        System.out.print("Введите оператор (+, -, *, /): ");
+        op = reader.next().charAt(0);
+        System.out.print("Введите 2 число: ");
+        num2 = reader.nextDouble();
+
+        switch(op) {
+            case '+': ans = num1 + num2;
+                break;
+            case '-': ans = num1 - num2;
+                break;
+            case '*': ans = num1 * num2;
+                break;
+            case '/': ans = num1 / num2;
+                break;
+            default:  System.out.printf("Ошибка! Введен некорректный оператор");
+                return;
+        }
+        System.out.print("Результат:\n");
+        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+    }
+}
