36ac487e20074a0ad73552524b6c53f94b0cf90f
Profi <faris-d@mail.ru>
2023-03-08 21:09:58 +0300

    Исправил вебинар 2 задача 2

--- Webinars/Webinar2/W2_PressString.java
+++ Webinars/Webinar2/W2_PressString.java
@@ -7,22 +7,21 @@ import java.util.Scanner;
 
 public class W2_PressString {
     public static void main (String[] args) {
-        String arr = "aaaabbbcccde";
-        char[] ch = arr.toCharArray();
-        int count = 1;
-
-        StringBuilder newText = new StringBuilder();
-        for (int i = 0; i < arr.length()-1; i++) {
-            if (ch[i] == ch[i+1]) {
+        String inChar = "aaaabbbcccdeeйй";
+        char[] arrChar = inChar.toCharArray();  // Создаем массив char из строки
+        StringBuilder sb = new StringBuilder(); // Создаем пустую строку
+        int count = 1;                          // Счетчик букв
+        for (int i = 0; i < arrChar.length-1; i++) {
+            if (arrChar[i] == arrChar[i+1]) {
                 count++;
             } else {
-                String countString = String.valueOf(count);
-                //newText.append(ch[i] + countString);
-                count = 1;
+                sb.append(arrChar[i]).append(count); // в строку добавляем букву и цифру
+                count = 1;                           // счетчик в исходное состояние
             }
+            if (i + 1 == arrChar.length - 1) {       // Когда i - предпоследний элемент
+                sb.append(arrChar[i + 1]).append(count); // в строку добавляем букву и цифру
+            }                                        // поскольку это завершающий элемент
         }
-        newText.append(ch[ch.length - 1] + String.valueOf(count));
-        System.out.println(newText);
-
+        System.out.println(sb);                      // Выводим результат
     }
 }
