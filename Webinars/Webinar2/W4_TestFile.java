e6a91444820f0a0851a5f89eae0b4ec463c57dc7
Profi <faris-d@mail.ru>
2023-03-08 22:31:54 +0300

    Исправил вебинар 2 задача 3 и 4

--- Webinars/Webinar2/W4_TestFile.java
+++ Webinars/Webinar2/W4_TestFile.java
@@ -4,16 +4,49 @@
 
 package Webinars.Webinar2;
 
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class W4_TestFile {
-    public static void main(String[] args) {
-        File file = new File("file.txt");
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите слово: ");
-        String str = sc.nextLine();
-        System.out.println(str);
-        //file.createNewFile();
+//    public static void main(String[] args) {
+//        File file = new File("test_file.txt");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Введите слово: ");
+//        //StringBuilder sb = new StringBuilder();
+//        String str = sc.nextLine();
+//        //sb.append(str.repeat(100));
+//
+//        try (FileWriter fw = new FileWriter(file, false)) {
+//            for (int i = 0; i < 100; i++) {
+//                fw.write(str + '\n');
+//            }
+//            fw.flush();
+//
+//            //--- Чтение построчно ---
+//            BufferedReader br = new BufferedReader(new FileReader(file));
+//            String str1;
+//            while ((str1 = br.readLine()) != null) {
+//                System.out.printf("== %s == \n", str1);
+//            }
+//        } catch (IOException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//    }
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            sb.append("QQ - WW \n");
+        }
+        metod(sb);
+    }
+
+    private static void metod (StringBuilder s) throws IOException {
+        try (FileWriter fw = new FileWriter("test_file.txt", false)) {
+            fw.write(s.toString());
+            fw.flush();
+        } catch (IOException ex) {
+            //System.out.println(ex.getMessage());
+            throw new IOException(ex.getMessage());
+        }
     }
 }
