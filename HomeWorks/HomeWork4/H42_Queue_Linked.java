b62a77e74c1e43e29af4ad89b805e46ba967d10c
Profi <faris-d@mail.ru>
2023-03-14 16:38:54 +0300

    Решил задачу 2 семинара 4

--- HomeWorks/HomeWork4/H42_Queue_Linked.java
+++ HomeWorks/HomeWork4/H42_Queue_Linked.java
@@ -6,8 +6,41 @@
  ***********************************************************************/
 package HomeWorks.HomeWork4;
 
+import java.util.LinkedList;
+
 public class H42_Queue_Linked {
     public static void main(String[] args) {
-
+        LinkedList<Integer> que = new LinkedList<>();
+        for (int i = 2; i < 5; i++) {
+            que.add(i);
+        }
+        System.out.println(que);
+        que = enqueue(que, 15);     // Помещаем новый элемент в конец очереди
+        System.out.println(que);
+        int a = dequeue(que);                 // Возвращаем первый элемент из очереди и удаляем его
+        System.out.print(a + " ");
+        System.out.println(que);
+        a = first(que);                         // Возвращаем первый элемент из очереди, не удаляя его
+        System.out.print(a + " ");
+        System.out.println(que);
+    }
+    /*****************************************************
+     * Помещает новый элемент в конец очереди           */
+    public static LinkedList<Integer> enqueue (LinkedList<Integer> inLinkedList, Integer newElement) {
+        inLinkedList.add(newElement);
+        return inLinkedList;
+    }
+    /*******************************************************
+     *  Возвращает первый элемент из очереди и удаляет его */
+    public static Integer dequeue (LinkedList<Integer> inLinkedList) {
+        Integer temp = inLinkedList.pollFirst();
+        return temp;
+    }
+    /********************************************************
+     *  Возвращает первый элемент из очереди, не удаляя его */
+    public static Integer first (LinkedList<Integer> inLinkedList) {
+        Integer temp = inLinkedList.getFirst();
+        return temp;
     }
 }
+
