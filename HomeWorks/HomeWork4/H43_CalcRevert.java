fa0373282fc67dd20ab687b3c642f7792b5e95e8
Profi <faris-d@mail.ru>
2023-03-20 23:11:16 +0300

    Записал урок 6

--- HomeWorks/HomeWork4/H43_CalcRevert.java
+++ HomeWorks/HomeWork4/H43_CalcRevert.java
@@ -15,40 +15,46 @@ import java.util.logging.SimpleFormatter;
 public class H43_CalcRevert {
     public static void main(String[] args) throws IOException {
 
-        double num1;
-        double num2;
-        double ans;
-        char op;
-
-        Stack<Double> st = new Stack<>();
-
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        ans = num1 = reader.nextDouble();
-        System.out.print("Введите оператор (+, -, *, /): ");
-        op = reader.next().charAt(0);
-        System.out.print("Введите второе число: ");
-        num2 = reader.nextDouble();
-
-        switch(op) {
-            case '+': ans += num2;
-                break;
-            case '-': ans -= num2;
-                break;
-            case '*': ans *= num2;
-                break;
-            case '/':
-                if (num2 == 0) {
-                    System.out.println("На ноль делить нельзя");
-                    return;
-                } else {
-                    ans /= num2;
-                }
-                break;
-            default:  System.out.printf("Ошибка! Введен некорректный оператор");
-                return;
-        }
-        System.out.print("Результат:\n");
-        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
+//        double num1;
+//        double num2;
+//        double ans;
+//        char op;
+//
+//        Stack<Double> st = new Stack<>();
+//
+//        Scanner reader = new Scanner(System.in);
+//        System.out.print("Введите первое число: ");
+//        ans = num1 = reader.nextDouble();
+//        while (true) {
+//            System.out.print("Введите оператор (+, -, *, /): ");
+//            op = reader.next().charAt(0);
+//            System.out.print("Введите второе число: ");
+//            num2 = reader.nextDouble();
+//
+//            switch (op) {
+//                case '+':
+//                    ans += num2;
+//                    break;
+//                case '-':
+//                    ans -= num2;
+//                    break;
+//                case '*':
+//                    ans *= num2;
+//                    break;
+//                case '/':
+//                    if (num2 == 0) {
+//                        System.out.println("На ноль делить нельзя");
+//                        return;
+//                    } else {
+//                        ans /= num2;
+//                    }
+//                    break;
+//                default:
+//                    System.out.printf("Ошибка! Введен некорректный оператор");
+//                    return;
+//            }
+//        }
+//        System.out.print("Результат:\n");
+//        System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
     }
 }
